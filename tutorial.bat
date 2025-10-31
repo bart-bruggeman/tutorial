351216f14b59f6bdd8a7d1557e642a7a937970d4
barbru <bart.bruggeman@kava.be>
2025-10-31 11:34:05 +0100

    parameterize scripts

--- tutorial.bat
+++ tutorial.bat
@@ -2,6 +2,20 @@
 cls
 setlocal enabledelayedexpansion
 
+:: check if a specific file parameter is given
+if "%~1"=="" (
+    :: no parameter, process all XML files
+    set "XML_FILES=data\*.xml"
+) else (
+    :: parameter given, process only that file in data directory
+    if exist "data\%~1.xml" (
+        set "XML_FILES=data\%~1.xml"
+    ) else (
+        echo File data\%~1.xml not found!
+        exit /b 1
+    )
+)
+
 :: versions
 set "SAXON_HE_VERSION=12.8"
 set "XML_RESOLVER_VERSION=5.3.3"
@@ -18,8 +32,8 @@ set "XML_RESOLVER_JAR=%SAXON_HE_HOME%\xmlresolver-%XML_RESOLVER_VERSION%.jar"
 
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
 
-:: validate and process all XML files
-for %%F in (data\*.xml) do (
+:: validate and process XML files
+for %%F in (%XML_FILES%) do (
     echo Processing %%F...
     :: validate XML tutorial
     java -cp "%XMLVALIDATOR_HOME%" be.bruggeman.cv.XmlValidator data/schema/tutorial.xsd "%%F"
